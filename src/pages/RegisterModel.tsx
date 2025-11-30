1b704df66e2041c1735302b4a5b33cb99a26d054
Kate Beker <katebeker@MacBook-Air-van-Kate.local>
2025-12-01 00:02:56 +0100

    Verbeterde auto-slash: direct na 2 cijfers een /

--- src/pages/RegisterModel.tsx
+++ src/pages/RegisterModel.tsx
@@ -5,9 +5,11 @@ import MiesLogo from '@/components/MiesLogo';
 export default function RegisterModel() {
   // Automatisch '/' toevoegen bij geboortedatum invoer
   const handleBirthdateInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let value = e.target.value.replace(/[^0-9]/g, '');
-    if (value.length > 2 && value[2] !== '/') value = value.slice(0,2) + '/' + value.slice(2);
-    if (value.length > 5 && value[5] !== '/') value = value.slice(0,5) + '/' + value.slice(5);
+    let value = e.target.value;
+    // Voeg automatisch '/' toe na dag en maand
+    value = value.replace(/[^0-9]/g, '');
+    if (value.length > 2) value = value.slice(0,2) + '/' + value.slice(2);
+    if (value.length > 5) value = value.slice(0,5) + '/' + value.slice(5);
     if (value.length > 10) value = value.slice(0,10);
     setFormData({ ...formData, birthdate: value });
   };
